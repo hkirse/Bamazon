1a5b60f952f9db3d341986da72f90fbc1ddf2316
Henry Kirse <hkirse@gmail.com>
2018-05-21 08:17:52 -0400

    final customer version

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,88 +1,62 @@
-var mysql = require('mysql');
-var inquirer = require("inquirer");
+// set up npms
+var inquirer = require("inquirer")
+var mysql = require("mysql")
 
-//declaring variable to be used to display amount of money owed
-var amountOwed;
-
+//set up connection to mySql db. don't forget curly brackets!argh
 var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: 'DurhamPortland910',
-    database: "bamazon"
+    host:"localhost", 
+    user:"root",
+    password:"DurhamPortland910",
+    database:"bamazon"
+})
+connection.connect(function(err){
+    if(err) throw err
+
 })
 
-connection.connect(function(err) {
-    if (err) throw err;
-    start();
-})
-
-//Starting display screen with all products viewable
-var start = function() {
-    connection.query("SELECT * FROM products", function(err, results){
-		if (err) throw err;
-
-		console.log('--------------------Welcome to Bamazon!!!  Your one stop shop for random stuff!!!---------------------')
+connection.query("SELECT * FROM products;", function(err, results){
+    console.log('--------------------Welcome to Bamazon!!!  Your one stop shop for random stuff!!!---------------------')
 		  console.log('----------------------------------------------------------------------------------------------------')
-			console.log('----------------------------------------------------------------------------------------------------')
-
-		for (var i = 0; i < results.length; i++){
-			console.log("ID: " + results[i].item_id + " | " + "Product: " + results[i].product_name + " | " + "Department: " + results[i].department_name + " | " + "Price: " + results[i].price + " | " + "QTY: " + results[i].stock_quantity);
-			console.log('--------------------------------------------------------------------------------------------------');
-		}
-        shoppingCart();
-    })
-};
-
-//starting inquirer prompts for product and quantity selection
-var shoppingCart = function() {
-    inquirer.prompt([{ 
-        name: "itemID",
+            console.log('----------------------------------------------------------------------------------------------------')
+            
+            for (var i = 0; i < results.length; i++){
+                console.log("ID: " + results[i].item_id + " | " + "Product: " + results[i].product_name + " | " + "Department: " + results[i].department_name + " | " + "Price: " + results[i].price + " | " + "QTY: " + results[i].stock_quantity);
+                console.log('--------------------------------------------------------------------------------------------------');
+            }
+             
+    inquirer.prompt([
+        {
+        name: "question1",
         type: "input",
-        message: "What is the ID of the product you like to purchase?",
-      
-        //Must validate if input is numerical value
-        validate: function(value) {
-            if (isNaN(value) == false) {
-                return true;
-            } else {
-                return false;
+        message: "What is the Id of the product you want to purchase?"
+        }, 
+        {
+        name: "question2",
+        type: "input",
+        message: "How many would you like to purchase?"
+        } 
+    ]).then (function(answers){
+ 
+        for (var i=0; i< results.length; i++) {
+            if(answers.question1==results[i].item_id) {
+                if(results[i].stock_quantity>=answers.question2) {
+                    var totalCost = answers.question2 * results[i].price
+                    var newQuantity = results[i].stock_quantity - answers.question2
+                    var name = results[i].product_name
+                    connection.query("UPDATE products SET ? WHERE ? ", [{stock_quantity: newQuantity},{item_id:answers.question1}], function(err, results){
+                        if(err) throw err
+                        
+                        console.log("Thank you for your order! Your order of "+ answers.question2 + " " + name + " is now being processed.");
+                        console.log("Total cost: $" + totalCost);
+                        console.log("total remaining in stock: " + newQuantity);
+                        connection.end()
+                    }) 
+                }else {
+                    console.log("Sorry .. not enough in stock");
+                }
+
             }
         }
-        
-    }, {
-        name: "Quantity",
-        type: "input",
-        message: "What quantity of the product would you like?",
-        validate: function(value) {
-            if (isNaN(value) == false) {
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        
-        }]).then(function(answer) {
-            var query = 'SELECT * FROM Products WHERE item_id =' + answer.itemID;
-            connection.query(query, function(err, res) {
-                if (answer.Quantity <= res) {                    
-                    for (var i = 0; i < res.length; i++) {
-                        //equation for the amount owed depending on the product and quantity chosen
-                        //amountOwed = res[0].price * answer.Quantity;
-                        console.log("We currently have " + res[i].stock_quantity + " " + res[i].product_name + ".");
-                        console.log("Thanks for your order! Your order of "+ answer.Quantity + " " + res[i].product_name + " is now being processed.");
-                        console.log('You owe $' + amountOwed);
 
-                //update stock if selection is available 
-                // connection.query('UPDATE products SET stock_quantity = ' + (res[i].stock_quantity - answer.Quantity) + ' WHERE item_id = ' + answer.itemID, [{
-                
-                
-                // }]); 
-                    }
-                } else {
-                    console.log("Sorry! We don't have enough in stock.");
-                }
-                start();
-            })
-        })
-    };
+    })
+})
